ca05c3b7ed3f781269c0a16d705c6c110cc271f7
ikeref <120581057+ikeref@users.noreply.github.com>
2022-12-14 22:08:41 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+window.onload = function() {
+  if (!localStorage.getItem('mcname')) {
+    console.log('no name set')
+    return
+  } else {
+    let mc = localStorage.getItem('mcname')
+    document.body.innerHTML = document.body.innerHTML.replaceAll(MC/g, mc)
+  }
+}
+
+function saveName() {
+  localStorage.setItem('mcname', charname.value);
+  document.getElementById('confirmtext').innerHTML = 'Main Character name successfully saved as "' + localStorage.getItem('mcname') + '"!';
+}
+
+function showR1() {
+  if (document.getElementsByClassName("r1")[0].style.display != "flex" && document.getElementsByClassName("r2")[0].style.display != "flex") {
+    document.getElementsByClassName("r1")[0].style.display = "flex";
+  } else if (document.getElementsByClassName("r1")[0].style.display != "flex") {
+    document.getElementsByClassName("r1")[0].style.display = "flex";
+    document.getElementsByClassName("r2")[0].style.display = "none";
+  } else {
+    document.getElementsByClassName("r1")[0].style.display = "none";
+  }
+}
+
+function showR2() {
+  if (document.getElementsByClassName("r1")[0].style.display != "flex" && document.getElementsByClassName("r2")[0].style.display != "flex") {
+    document.getElementsByClassName("r2")[0].style.display = "flex";
+  } else if (document.getElementsByClassName("r2")[0].style.display != "flex") {
+    document.getElementsByClassName("r2")[0].style.display = "flex";
+    document.getElementsByClassName("r1")[0].style.display = "none";
+  } else {
+    document.getElementsByClassName("r2")[0].style.display = "none";
+  }
+}
